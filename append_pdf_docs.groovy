ba20ad19c77b9958a6ca61bce5c8ef027fdfe0dc
mbohun <martin.bohun@gmail.com>
2019-07-12 15:51:04 +1000

    MILESTONE: added index/annotations (first page, ToC) and it partialy works

--- append_pdf_docs.groovy
+++ append_pdf_docs.groovy
@@ -6,16 +6,21 @@ import java.io.File
 import java.io.IOException
 
 import org.apache.pdfbox.io.MemoryUsageSetting
-
-import org.apache.pdfbox.pdmodel.PDPage
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.PDDocumentInformation
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.PDPageContentStream
 import org.apache.pdfbox.pdmodel.PageMode
+import org.apache.pdfbox.pdmodel.common.PDRectangle
+import org.apache.pdfbox.pdmodel.font.PDFont
+import org.apache.pdfbox.pdmodel.font.PDType1Font
+import org.apache.pdfbox.pdmodel.interactive.action.PDActionGoTo
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLink
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDBorderStyleDictionary
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageDestination
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageFitWidthDestination
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem
-
 import org.apache.pdfbox.multipdf.PageExtractor
 import org.apache.pdfbox.multipdf.PDFMergerUtility
 
@@ -29,12 +34,7 @@ if (this.args.length < 1) {
     return -1
 }
 
-def final doc_to_append = []
-
-new File(this.args[0]).eachLine {
-    doc_to_append << it
-}
-
+def final doc_to_append = new File(this.args[0]).collect { it }
 doc_to_append.forEach {
     println "INPUT file found: ${it}"
 }
@@ -54,8 +54,10 @@ try {
     }
     println "PDFs number of pages: ${pdfs_number_pages}; total: ${pdfs_number_pages.sum()}"
 
-    def start_page = 0 as Integer
-    def final startPageDocName = [:]
+    //NOTE: 1 because we are prepeding
+
+    def start_page = 1 as Integer
+    def final startPageDocName = [0: 'index']
     doc_to_append.eachWithIndex { it, i ->
         startPageDocName[start_page] = it // TODO: CLEANUP: make a list of page offsets
         start_page += pdfs_number_pages[i]
@@ -68,6 +70,14 @@ try {
     // TODO: create header from index.html, that will give you header_lenght (in pages)
     def final mem = MemoryUsageSetting.setupMainMemoryOnly()
     def final result = new PDDocument(mem) //TEST: MemoryUsageSetting.setupMainMemoryOnly()
+
+    // create the index page and doc
+    def final doc_index = new PDDocument()
+    doc_index.addPage(new PDPage())
+    //doc_index.close()
+
+    pdfMergerUtility.appendDocument(result, doc_index)
+
     pdfs.forEach {
         pdfMergerUtility.appendDocument(result,
                                         new PageExtractor(it).extract())
@@ -83,6 +93,7 @@ try {
     //       works *ONLY* pdfMergerUtility.addSource()
 
     createBookmarkPerAppendedDoc(result, startPageDocName) //[0:'test_input_a.pdf', 19: 'test_input_b.pdf', 37: 'test_input_c.pdf'])
+    createAnnotationPerAppendedDoc(result, startPageDocName)
 
     println "OUTPUT PDF number of pages (after createBookmarks): ${result.getNumberOfPages()}"
 
@@ -93,6 +104,7 @@ try {
 
 } catch (Exception e) {
     System.err.println("exception while trying to merge PDF doc: ${out_pdf}; exception: ${e}");
+    e.printStackTrace()
 }
 
 PDDocument createBookmarkPerAppendedDoc(final PDDocument document, final Map originalDocs) {
@@ -121,4 +133,63 @@ PDDocument createBookmarkPerAppendedDoc(final PDDocument document, final Map ori
     // optional: show the outlines when opening the file
     document.getDocumentCatalog().setPageMode(PageMode.USE_OUTLINES)
     return document
+}
+
+PDDocument createAnnotationPerAppendedDoc(final PDDocument document, final Map originalDocs) {
+        def final INCH = 72 as float
+
+        def final borderULine = new PDBorderStyleDictionary()
+        borderULine.setStyle(PDBorderStyleDictionary.STYLE_UNDERLINE)
+        borderULine.setWidth((INCH / 72) as float) // 1 point
+    //try {
+        // TODO: obviously this is a test, the real thing will be the links in the attachment table
+        def final page_index = document.getPage(0)
+        annotations_index = page_index.getAnnotations() //List<PDAnnotation
+
+        def final pw = page_index.getMediaBox().getUpperRightX() as float
+        def final ph = page_index.getMediaBox().getUpperRightY() as float
+        def final font = PDType1Font.HELVETICA_BOLD
+
+        def final contents = new PDPageContentStream(document, page_index)
+        contents.beginText()
+        contents.setFont(font, 18)
+
+        def final x = (ph - INCH - 18) as float
+        contents.newLineAtOffset(INCH, x) //?
+
+        originalDocs.each { docStartPage, docName ->
+            contents.newLineAtOffset(0 as float, -(INCH / 2) as float)
+            contents.showText(docName)
+        }
+
+        contents.endText()
+        contents.close()
+
+        originalDocs.each { docStartPage, docName ->
+            def final page = document.getPage(docStartPage)
+
+            def final dest = new PDPageFitWidthDestination()
+            dest.setPage(page)
+
+            def final pageLink = new PDAnnotationLink()
+            pageLink.setBorderStyle(borderULine)
+
+            // Set the rectangle containing the link
+            def final textWidth = font.getStringWidth(docName) / 1000 * 18 as float
+            def final position = new PDRectangle()
+            position.setLowerLeftX(INCH as float)
+            position.setLowerLeftY(ph - 2 * INCH - 20 as float)  // down a couple of points
+            position.setUpperRightX((INCH + textWidth) as float)
+            position.setUpperRightY(ph - 2 * INCH as float)
+            pageLink.setRectangle(position)
+
+            def final actionGoto = new PDActionGoTo()
+            actionGoto.setDestination(dest)
+            pageLink.setAction(actionGoto)
+
+            annotations_index.add(pageLink)
+        }
+
+      return document
+    //}
 }
