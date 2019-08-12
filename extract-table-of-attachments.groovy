f14ba10bf488211fe9f1de5d8ecb3cd8a7b5b729
mbohun <martin.bohun@gmail.com>
2019-08-12 12:12:48 +1000

    CLEANUP/TEST: more complex/full example of how to use PDFTextStripper + string search to find PDPage-s of interest (this is useful later to restrict certain PDFBox operations like search-and-replace of annotations only to certain page-s, resp. page range-s).

--- extract-table-of-attachments.groovy
+++ extract-table-of-attachments.groovy
@@ -11,6 +11,8 @@ import org.apache.pdfbox.text.PDFTextStripper
 import java.io.File
 import java.io.IOException
 
+import groovy.json.JsonOutput
+
 // NOTE: XPath numbering/indexing starts from 1, so the table of attachments is index 26 in XPath.
 //       However the XPath-like groovy.util.slurpersupport.GPathResult uses numbering/indexing
 //       starting from 0, hence the table of attachments is index 25 in GPathResult.
@@ -68,37 +70,57 @@ attachments.collect { it['input'] }.each { println it }
 def final htmlDocPdf = PDDocument.load(new File("test_input-00/Patient No 2 Medical Record/Patient No 2 Medical Record_fixed_adj.pdf"))
 def final pdfTextStripper = new PDFTextStripper()
 
-// TEST: we will print the page numbers of the pages containing these strings
-def final searchTerms = [
-    'Registration Details - Patient No: 2',
-    'Attachments',
-    'Due Diary Entries'
-]
-
-def st = searchTerms.pop()
-
 // NOTE: Normally this would be .size() - 1 BUT PDFBox does *NOT* use
 //       the element index 0 in the List of PDPage (in the PDDocument) that is
 //       to start numbering pages from 1 (and not from 0).
 //
 def final lastPageno = htmlDocPdf.getPages().size() // or: htmlDocPdf.getNumberOfPages()
 println "lastPageno=${lastPageno}"
+println ""
+
+// TEST: We will print the page numbers of the pages containing these strings.
+//       Some strings/expressions are simply too generic/common (for a patient
+//       record, example: ^Medication$
+//       That is the reason why we have to use long-er strings to actually
+//       match the exact place in the patient record.
+//
+def final searchTerms = [
+    "\nRegistration Details - Patient No: 2\n",
+    "\nAttachments Authorised By Code\n",
+    "\nDue Diary Entries Authorised By Code\n",
+    "\nConsultations\n",
+    "\nMedication\nCurrent\nDate Commenced Drug Details Date Last Issue Authorised By Type\n"
+]
+
+// NOTE: I am using/abusing here JSON output for clarity/emphasis to display all the '\n'
+//       in the searchTerms string-s.
+//
+println "searchTerms: ${JsonOutput.prettyPrint(JsonOutput.toJson(searchTerms))}"
+
+def st = searchTerms.pop()
+println "searching for: ${JsonOutput.toJson(st)}"
 
 // NOTE: This is a *VERY* constrained version, that works only if:
 //       - the searchTerms (st) are ordered in the order of their appearance in the PDF
 //       - each search term (st) is either UNIQUE (or YOU are interested ONLY in the first match)
 //
 htmlDocPdf.getPages().eachWithIndex { page, pageno ->
-    println "searching for: ${st}"
-    println "    DEBUG: pageno=${pageno}"
+    //println "searching for: ${st}"
+    //println "        DEBUG: pageno=${pageno}"
     pdfTextStripper.setStartPage(pageno)
     pdfTextStripper.setEndPage(pageno)
     def final text = pdfTextStripper.getText(htmlDocPdf)
 
-    if (text.contains(st)) {
-        println "${st} found on page: ${pageno}"
+    // NOTE: Uncomment this if you want to see/read/examine the actual PDPage
+    //       text (String) returned by the PDFTextStripper.
+    //
+    //println "pageno=${pageno}\n${text}"
+
+    if (text.indexOf(st) > -1) {
+        println "        FOUND: ${JsonOutput.toJson(st)} on page: ${pageno}"
         if (searchTerms.size()) {
             st = searchTerms.pop()
+            println "searching for: ${JsonOutput.toJson(st)}"
         } else {
             System.exit(0)
         }
@@ -106,9 +128,10 @@ htmlDocPdf.getPages().eachWithIndex { page, pageno ->
 
     // NOTE: this should never happen to us, we basically reached the last page and there was NO match
     if (pageno == lastPageno) {
-        println "${st} not found!"
+        println "        ${JsonOutput.toJson(st)} NOT FOUND!"
         if (searchTerms.size()) {
             st = searchTerms.pop()
+            println "searching for: ${JsonOutput.toJson(st)}"
         } else {
             System.exit(0)
         }
