d040d67124d2e7260f6f5adfbd0b119d2db217d8
mbohun <martin.bohun@gmail.com>
2019-08-23 12:31:57 +1000

    INIT: first simple working version; HOWEVER: this version works only with well formatted HTML input (and that is obviously *NOT* the case most of the time, so the next version has to add a sanitization step that will adjust/fix not-well formed HTML before processing (with itextpdf).

--- html-to-pdf-iText.groovy
+++ html-to-pdf-iText.groovy
@@ -1,12 +1,26 @@
+#!/usr/bin/env groovy
+
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
 
+@Grab('com.itextpdf:itextpdf:5.5.13.1')
 import com.itextpdf.text.Document
 import com.itextpdf.text.DocumentException
 import com.itextpdf.text.pdf.PdfWriter
+
+@Grab(group='com.itextpdf.tool', module='xmlworker', version='5.5.13.1')
 import com.itextpdf.tool.xml.XMLWorkerHelper
 
+if (this.args.length < 1) {
+    println '''
+        ERROR: no input HTML file
+    '''
+    return -1
+}
+
+def final htmlFileName = this.args[0]
+def final outputPdf = htmlFileName + '_' + System.currentTimeMillis() + '.pdf'
 
 def final pdfDoc = new Document()
 def final pdfWriter = PdfWriter.getInstance(pdfDoc, new FileOutputStream(outputPdf))
@@ -16,3 +30,5 @@ XMLWorkerHelper.getInstance().parseXHtml(pdfWriter,
                                          pdfDoc,
                                          new FileInputStream(htmlFileName))
 pdfDoc.close()
+
+println "OUTPUT: ${outputPdf}"
