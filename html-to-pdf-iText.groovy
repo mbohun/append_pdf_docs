8cc84e7a6d17757080cd1c8cf16f6831e35f6cf5
mbohun <martin.bohun@gmail.com>
2019-09-03 17:25:23 +1000

    FIX: do the <c> tag-s replacement on String loaded form the HTML input; include the log4j libs to shutup the console messages.

--- html-to-pdf-iText.groovy
+++ html-to-pdf-iText.groovy
@@ -6,6 +6,8 @@ import groovy.xml.XmlUtil
 import org.cyberneko.html.parsers.SAXParser
 
 @Grapes([
+    @Grab(group='org.apache.logging.log4j', module='log4j-api', version='2.12.1'),
+    @Grab(group='org.apache.logging.log4j', module='log4j-slf4j-impl', version='2.12.1', scope='test'),
     @Grab(group='com.itextpdf', module='itext7-core', version='7.1.7', type='pom'),
     @Grab(group='com.itextpdf', module='html2pdf', version='2.1.4')
 ])
@@ -32,13 +34,14 @@ def final parser = new SAXParser()
 parser.setFeature("http://cyberneko.org/html/features/augmentations", true);
 //parser.setFeature("http://cyberneko.org/html/features/balance-tags/document-fragment", true)
 parser.setProperty("http://cyberneko.org/html/properties/names/elems", "lower");
-def final doc = new XmlSlurper(parser).parse(new File(htmlFileName))
 
 // NOTE: Remove the <c></c> tags/elements, because itextpdf doesn't undertand those
 //       and it removes them including the text enclosed in <c></c> and that is
 //       something we clearly do NOT want
 //
-def final doc_fixed = XmlUtil.serialize(doc).replace("<c>", "").replace("</c>", "")
+def final htmlCode = new File(htmlFileName).getText("WINDOWS-1252").replace("<c>", "").replace("</c>", "") //.replace("<c/>", "")
+def final doc = new XmlSlurper(parser).parseText(htmlCode)
+def final doc_fixed = XmlUtil.serialize(doc)
 
 // NOTE: Saving the intermediate fixed/adjusted HTML file for review/examination
 // of the final PDF.
