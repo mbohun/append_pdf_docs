f9323f463c085445967924424b00005f57e2d076
mbohun <martin.bohun@gmail.com>
2019-09-06 14:48:32 +1000

    CLEAN: added CliBuilder (CLI args parsing), and moved/scoped the existing functionality (extract textx, search in text) into a dedicated function. This is to allow for easy extensions/additions. Added simple error handling.

--- pdfbox-tool.groovy
+++ pdfbox-tool.groovy
@@ -9,71 +9,86 @@ import java.io.File
 import java.io.IOException
 
 import groovy.json.JsonOutput
+import groovy.cli.commons.CliBuilder
+import groovy.cli.commons.OptionAccessor
 
-if (this.args.length < 1) {
-    println '''
-        ERROR: no input PDF file
-    '''
-    return -1
+def final cli = new CliBuilder(usage: 'pdfbox-tool.groovy [options] <PDF file>')
+cli.h(longOpt: 'help', 'display usage')
+cli.t(longOpt: 'text-extract', 'extract the text from PDF doc', args: 1, type: String)
+
+def final options = cli.parse(args)
+
+if (options.h) {
+    cli.usage()
+    System.exit(0)
 }
 
-def final pdfFileName = this.args[0]
-def final pdf = PDDocument.load(new File(pdfFileName))
+def final result = [:]
 
-def final result = [
-    "doc": pdfFileName,
-    "pages": [],
-    "page-start-offsets": []
-]
-
-def offset = 0
-def final allPagesTextBuffer = new StringBuffer()
-
-def final pdfTextStripper = new PDFTextStripper()
-def final pdfToList = pdf.getPages().eachWithIndex { page, pageno ->
-    pdfTextStripper.setStartPage(pageno)
-    pdfTextStripper.setEndPage(pageno)
-
-    def final text = pdfTextStripper.getText(pdf) 
-    result["pages"] << text
-
-    // NOTE: We are merging all page-s text into one StringBuffer (String).
-    //       This is required for exact/accurate searching for strings in the
-    //       text.
-    //       Naive approach is to search for a given string in a text of
-    //       one page, BUT that FAILS if your string is pread across multiple
-    //       (usually 2 pages). This way we merge/append all pages (text)
-    //       into one String, save the page start offset-s into a list of
-    //       offsets, then we search for our string-s in the merged string,
-    //       and use the list of offsets to identify on which page the string
-    //       we are searching for starts.
-    //
-    allPagesTextBuffer << text
-
-    result["page-start-offsets"] << offset
-    offset += text.length()
-    
-}
-
-def final searchTerms = [
-    "\nAttachments Authorised By Code\n",
-    "\nDue Diary Entries Authorised By Code\n"
-]
-
-result["search-terms-offsets"] = searchTerms.collect {
-    def final pos = allPagesTextBuffer.indexOf(it) // we could continue the search from the last match position
-    if (pos > -1) {
-        def final i = result["page-start-offsets"].findIndexOf { it >= pos }
-        if (pos == result["page-start-offsets"][i]) {
-            //println "DEBUG: searchTerm=${it}; pos=${pos}; page=${i}"
-            i
-        } else {
-            //println "DEBUG: searchTerm=${it}; pos=${pos}; page=${i-1}"
-            i - 1
-        }
-    } else {
-        -1 // pos; searchTerm not found
+try {
+    if (options['text-extract']) {
+        result['doc'] = options['text-extract']
+        textExtract(result)
     }
+
+} catch (Exception e) {
+    result['error'] = e.getMessage()
 }
 
 println "${JsonOutput.prettyPrint(JsonOutput.toJson(result))}"
+
+def textExtract(result) {
+    def final pdf = PDDocument.load(new File(result['doc']))
+    
+    result["pages"] = []
+    result["page-start-offsets"] = []
+
+    def offset = 0
+    def final allPagesTextBuffer = new StringBuffer()
+
+    def final pdfTextStripper = new PDFTextStripper()
+    def final pdfToList = pdf.getPages().eachWithIndex { page, pageno ->
+        pdfTextStripper.setStartPage(pageno)
+        pdfTextStripper.setEndPage(pageno)
+
+        def final text = pdfTextStripper.getText(pdf)
+        result["pages"] << text
+
+        // NOTE: We are merging all page-s text into one StringBuffer (String).
+        //       This is required for exact/accurate searching for strings in the
+        //       text.
+        //       Naive approach is to search for a given string in a text of
+        //       one page, BUT that FAILS if your string is pread across multiple
+        //       (usually 2 pages). This way we merge/append all pages (text)
+        //       into one String, save the page start offset-s into a list of
+        //       offsets, then we search for our string-s in the merged string,
+        //       and use the list of offsets to identify on which page the string
+        //       we are searching for starts.
+        //
+        allPagesTextBuffer << text
+
+        result["page-start-offsets"] << offset
+        offset += text.length()
+    }
+
+    def final searchTerms = [
+        "\nAttachments Authorised By Code\n",
+        "\nDue Diary Entries Authorised By Code\n"
+    ]
+
+    result["search-terms-offsets"] = searchTerms.collect {
+        def final pos = allPagesTextBuffer.indexOf(it) // we could continue the search from the last match position
+        if (pos > -1) {
+            def final i = result["page-start-offsets"].findIndexOf { it >= pos }
+            if (pos == result["page-start-offsets"][i]) {
+                //println "DEBUG: searchTerm=${it}; pos=${pos}; page=${i}"
+                i
+            } else {
+                //println "DEBUG: searchTerm=${it}; pos=${pos}; page=${i-1}"
+                i - 1
+            }
+        } else {
+            -1 // pos; searchTerm not found
+        }
+    }
+}
